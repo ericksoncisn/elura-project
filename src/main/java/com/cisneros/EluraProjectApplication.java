49bb0b287980d6a5c7aabfea776ff6c94cb0a3c1
erici <ericiba99@gmail.com>
2025-11-13 13:42:37 -0500

    create initial MVC package structure

---
+++ src/main/java/com/cisneros/EluraProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cisneros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EluraProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EluraProjectApplication.class, args);
+	}
+
+}
